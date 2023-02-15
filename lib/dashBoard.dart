53ebf450aceb40e8c510efaf64d0313ab9551616
itamare <cladelphi007@gmail.com>
2023-02-15 07:38:46 -0300

    primeiro commit

---
+++ lib/dashBoard.dart
@@ -0,0 +1,17 @@
+// ignore: file_names
+// ignore: file_names
+import 'package:flutter/material.dart';
+
+class DashBoard extends StatelessWidget {
+  const DashBoard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Dashboard'),
+      ),
+      body: const Center(child: Text('Dashboard')),
+    );
+  }
+}
